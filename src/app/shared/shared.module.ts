04e863fc2f98fd56541698cacd8facdea0515bad
oDs1337 <odstobiaszcod@gmail.com>
2023-09-17 16:49:22 +0200

    add top nav bar with routing buttons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TopNavBarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
+  exports: [
+    TopNavBarComponent,
   ]
 })
 export class SharedModule { }
