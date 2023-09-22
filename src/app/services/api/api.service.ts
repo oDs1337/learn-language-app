9b18364e2b3e0eb47a01ea0afcedb7291e86e2fe
oDs1337 <odstobiaszcod@gmail.com>
2023-09-22 11:31:58 +0200

    add env

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Word } from 'src/app/shared/Interfaces/word';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,10 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getAllWords(): Observable<Word[]> {
-    return this.http.get<Word[]>('http://127.0.0.1:8000/get_all_words');
+    return this.http.get<Word[]>(`${environment.apiUrl}/get_all_words`);
   }
 
   postWord(payload: Word) {
-    return this.http.post<Word>('http://127.0.0.1:8000/add_word', payload);
+    return this.http.post<Word>(`${environment.apiUrl}/add_word`, payload);
   }
 }
